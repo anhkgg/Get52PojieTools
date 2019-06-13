b55c6b40639ffe6c12025475f69110fbf46026e0
anhkgg <anhkgg@163.com>
2019-06-13 13:11:02 +0800

    can work

---
+++ README.md
@@ -0,0 +1,59 @@
+
+# Get52PojieTools
+
+一个批量下载[52pojie](www.52pojie.cn)[爱盘](https://down.52pojie.cn/)中资源的小脚本。
+
+```
+爱盘地址：https://down.52pojie.cn/
+爱盘限制多线程下载访问，请使用单线程进行下载访问，多并发会被禁止访问。
+由于附件可能被安全软件误报，部分附件添加了压缩密码，默认解压密码：www.52pojie.cn
+```
+
+非常感谢吾爱破解整理提供的资源，包括教程、工具等。
+
+## 使用
+
+### 依赖
+
+```
+python 2.7
+pip
+pip install requests
+pip install simplejson
+```
+
+### 运行
+
+```
+python main.py
+```
+
+如果不想下载所有的资源，可自行指定要下载目录
+
+```
+filter = [
+    'Tools/Android_Tools/',
+]
+```
+
+运行结果：
+
+```
+*********************************
+* A downloader for 52pojie aipan 
+* Site: https://down.52pojie.cn  
+* Author: anhkgg
+* Site: github.com/anhkgg, anhkgg.com
+* CopyRight (c) 2019 
+*********************************
+ 
+ Start downloading ... 
+ You want to download these dir: 
+ -> Tools/
+ ...
+ Finish the job. 0.21s
+```
+
+## 赏杯咖啡
+
+![img](./pay.png)
---
+++ main.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+import sys, os
+import time
+import requests
+try:
+    import simplejson as json
+except:
+    import json
+
+
+def save_file(path, data):
+    f = file(path, 'w')
+    f.write(data)
+    f.close()
+
+def get_file_path(parent_dir, chi_list, path_list):
+    for chi in chi_list:
+        cur_path = os.path.join(parent_dir, chi['name'])
+        if chi.has_key('children'):
+            get_file_path(cur_path, chi['children'], path_list)
+        else:
+            path_list.append(cur_path[1:])
+
+def get_file_list():
+    url = 'https://down.52pojie.cn/list.js'
+    headers = {
+        'Host': 'down.52pojie.cn',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
+    }
+    res = requests.get(url, headers = headers)
+    data = res.content
+    data = data.replace('__jsonpCallbackDown52PojieCn(', '')
+    data = data.replace(');', '')
+
+    data = json.loads(data)
+    path_list = []
+    get_file_path('', [data], path_list)
+
+    return path_list
+
+def download(pdir, url):
+    try:
+        path = os.path.join(pdir, url)
+        if os.path.exists(path):
+            return
+        dir1 = os.path.dirname(path)
+        if not os.path.exists(dir1):
+            os.makedirs(dir1)
+        url = u'https://down.52pojie.cn/' + url
+        print 'downloading ', url
+        res = requests.get(url)
+        data = res.content
+        save_file(path, data)
+    except Exception as e:
+        print e
+
+def filter_download(dir, filter, path_list):
+    download_list = []
+    for path in path_list:
+        ret = [f for f in filter if path.find(f) != -1]
+        if len(ret) > 0:
+            download_list.append(path)
+            download(dir, path)
+
+    data = '\n'.join(path for path in download_list)
+    save_file('download.txt', data.encode('utf8'))
+
+if __name__ == '__main__':
+    print '*********************************'
+    print '* A downloader for 52pojie aipan '
+    print '* Site: https://down.52pojie.cn  '
+    print '* Author: anhkgg'
+    print '* Site: github.com/anhkgg, anhkgg.com'
+    print '* CopyRight (c) 2019 '
+    print '*********************************'
+    
+    print ' \n Start downloading ... '
+    filter = [
+        'Tools/',
+    ]
+    print ' You want to download these dir: '
+    for f in filter:
+        print ' ->', f
+
+    start_time = time.time()
+    path_list = get_file_list()
+    pdir = os.getcwd()
+    filter_download(pdir, filter, path_list)
+    print ' Finish the job. %.2fs' % (time.time() - start_time)
